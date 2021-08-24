87bebd013d4de5b1ec3466f6c95549473e9cbd45
jwcrosby <jwcrosby89@gmail.com>
2021-08-23 21:33:37 -0600

    render data from postgresql database on index template

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Cryptid(models.Model):
+  name = models.CharField(max_length=100)
+  other = models.CharField(max_length=100)
+  description = models.TextField(max_length=250)
+  location = models.CharField(max_length=100)
+
+  def __str__(self):
+    return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,19 +1,5 @@
 from django.shortcuts import render
-
-# Add the Cat class & list and view function below the imports
-class Cryptid:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, other, description, location):
-    self.name = name
-    self.other = other
-    self.description = description
-    self.location = location
-
-cryptids = [
-  Cryptid('Bigfoot', 'tabby', 'Kinda rude.', 'newyork'),
-  Cryptid('Loch Ness', 'tortoiseshell', 'Looks like a turtle.', 'themoon'),
-  Cryptid('Chupacabra', 'bombay', 'Happy fluff ball.', 'paris'),
-  Cryptid('Liz', 'selkirk rex', 'Meows loudly.', 'mars')
-]
+from .models import Cryptid
 
 def home(request):
   return render(request, 'home.html')
@@ -22,4 +8,5 @@ def about(request):
   return render(request, 'about.html')
 
 def cryptids_index(request):
+  cryptids = Cryptid.objects.all()
   return render(request, 'cryptids/index.html', { 'cryptids': cryptids })
