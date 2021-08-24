35d6c47d7a97eb09586ca112452baf0cc9327859
jwcrosby <jwcrosby89@gmail.com>
2021-08-24 15:18:24 -0600

    hide login form on home template when user is logged in

--- cryptidcollector/settings.py
+++ cryptidcollector/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-LOGIN_REDIRECT_URL = 'cats_index'
+LOGIN_REDIRECT_URL = 'cryptids_index'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
