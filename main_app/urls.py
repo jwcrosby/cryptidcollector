9542d64f6559d474dd2655e69311e872629960e6
jwcrosby <jwcrosby89@gmail.com>
2021-08-23 20:43:41 -0600

    render index page

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
   path('', views.home, name="home"),
   path('about/', views.about, name="about"),
+  path('cryptids/', views.cryptids_index, name='cryptids_index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Add the Cat class & list and view function below the imports
+class Cryptid:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, other, description, location):
+    self.name = name
+    self.other = other
+    self.description = description
+    self.location = location
+
+cryptids = [
+  Cryptid('Lolo', 'tabby', 'Kinda rude.', 'newyork'),
+  Cryptid('Sachi', 'tortoiseshell', 'Looks like a turtle.', 'themoon'),
+  Cryptid('Fancy', 'bombay', 'Happy fluff ball.', 'paris'),
+  Cryptid('Bonk', 'selkirk rex', 'Meows loudly.', 'mars')
+]
+
 def home(request):
   return HttpResponse('<h1>Hello Creep</h1>')
 
 def about(request):
   return render(request, 'about.html')
+
+def cryptids_index(request):
+  return render(request, 'cryptids/index.html', { 'cryptids': cryptids })
