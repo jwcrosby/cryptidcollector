9872d050e3fb01e6dfd49f9f61f816eb4b0b7af5
jwcrosby <jwcrosby89@gmail.com>
2021-08-23 22:48:20 -0600

    implement update cryptid functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
   path('cryptids/', views.cryptids_index, name='cryptids_index'),
   path('cryptids/<int:cryptid_id>/', views.cryptids_detail, name='cryptids_detail'),
   path('cryptids/create/', views.CryptidCreate.as_view(), name='cryptids_create'),
+  path('cryptids/<int:pk>/update/', views.CryptidUpdate.as_view(), name='cryptids_update'),
+  path('cryptids/<int:pk>/delete/', views.CryptidDelete.as_view(), name='cryptids_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Cryptid
 
 def home(request):
@@ -19,4 +19,11 @@ def cryptids_detail(request, cryptid_id):
 class CryptidCreate(CreateView):
   model = Cryptid
   fields = '__all__'
-  
+
+class CryptidUpdate(UpdateView):
+  model = Cryptid
+  fields = ['other', 'description', 'location']
+
+class CryptidDelete(DeleteView):
+  model = Cryptid
+  success_url = '/cryptids/'
