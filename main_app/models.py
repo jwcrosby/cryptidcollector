c477f96f1d84ec68a3599cadd8392d237b778a97
jwcrosby <jwcrosby89@gmail.com>
2021-08-24 10:39:10 -0600

    add photo model

--- main_app/models.py
+++ main_app/models.py
@@ -54,3 +54,10 @@ class Sighting(models.Model):
 
   class Meta:
     ordering = ['-date']
+
+class Photo(models.Model):
+  url = models.CharField(max_length=250)
+  cryptid = models.OneToOneField(Cryptid, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"Photo for cryptid_id: {self.cryptid_id} @{self.url}"
