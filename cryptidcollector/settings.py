bcebef009aed070150df82b17f774c4b6bd37721
jwcrosby <jwcrosby89@gmail.com>
2021-08-24 15:11:43 -0600

    update nav bar with log in/out

--- cryptidcollector/settings.py
+++ cryptidcollector/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+LOGIN_REDIRECT_URL = 'cats_index'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
